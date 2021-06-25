3cd9b482fc1419cb5b7a593bc0afc66ef1dab313
cmur85 <39341418+cmurestudillos@users.noreply.github.com>
2021-06-25 08:57:54 +0200

    Actualizar aplicación a la versión 12 de Angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,7 +15,7 @@ const routes: Routes = [
 @NgModule({
   declarations: [],
   imports: [
-    RouterModule.forRoot(routes, {useHash: true})
+    RouterModule.forRoot(routes, { useHash: true, relativeLinkResolution: 'legacy' })
   ],
   exports: [
     RouterModule
